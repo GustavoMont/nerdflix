ae04ce23f6a43e3c679ebf9691f59d327d5c3e0e
GustavoMont <lgmluisgm@gmail.com>
2021-10-05 10:18:32 -0300

    :pencil2: add a unique key to MovieList

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { getContent } from './Utils/getContent'
 import MovieList from './Components/MovieList'
 import { GlobalStyle } from './styles'
+import Outdoor from './Components/Outdoor';
 
 
 function App() {
   const [content, setContent] = useState([])
+  const [pic, setPic] = useState(null)
+
 
   useEffect(() =>{
     (async ()=>{
       const conteudo = await getContent()
       setContent(conteudo)
+      setPic(conteudo[0].lista[0].backdrop_path)
     })()
   }, [])
 
@@ -19,8 +23,9 @@ function App() {
       <GlobalStyle />
       <div className="App">
         <>
+        {pic && <Outdoor pic={pic} />}
           {
-            content.map(item => <MovieList content={item} />)
+            content.map(item => <MovieList content={item} key={item.title}/>)
           }
         </>
       </div>
