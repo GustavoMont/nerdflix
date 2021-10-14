9fca1ef33f540fe02c77d0a0fd5c5538ddd13e24
GustavoMont <lgmluisgm@gmail.com>
2021-10-14 20:36:53 -0300

    :pencil: comment all pages

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,31 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom' // Component to redirect to home page
 import { Header } from '../styles/Header'
 import { GlobalStyle } from '../styles/GlobalStyle'
-import { GoogleLogin } from 'react-google-login';
-import Cookies from 'js-cookie'
+import { GoogleLogin } from 'react-google-login'; // lib to help with Google Auth2
+import Cookies from 'js-cookie' // lib to help with cookies
 
 
-
+// background image
 const url = "https://pipocasclub.com.br/wp-content/uploads/2019/10/netflix-3.jpg"
 
 const Login = () => {
     
-    const [loggedIn, setLoggedIn] = useState(false)
-    const [fail, setFail] = useState(false)
-    const [image, setImage] = useState(false)
-    const failGoogle = () => setFail(true)
-    const responseGoogle = (response) => {
-        const { imageUrl } = response.profileObj
-        setImage(imageUrl)
-        setLoggedIn(true)
+    const [loggedIn, setLoggedIn] = useState(false) // initial state of login
+    const [fail, setFail] = useState(false) // if the auth fail
+    const [image, setImage] = useState(false) // if get image
+    const failGoogle = () => setFail(true) // if auth fail it set fail to true
+    const responseGoogle = (response) => { // if auth be succed
+        const { imageUrl } = response.profileObj // will get only profile picture from the account
+        setImage(imageUrl) // will set image to this Url
+        setLoggedIn(true) // will set login to true
     }
     
-    document.body.onscroll = null
+    document.body.onscroll = null // cancel the scroll event
     
     useEffect(() => {
-        const cookieData = JSON.stringify({isLogged: loggedIn, imageUrl: image})
-        Cookies.set('nerdflix', cookieData)
+        const cookieData = JSON.stringify({isLogged: loggedIn, imageUrl: image}) // turn this object a JSON to set it in a Cookie
+        Cookies.set('nerdflix', cookieData) // set the nerdflix cookie with de data 
     }, [loggedIn, image])
     return (
         <>
@@ -41,9 +41,9 @@ const Login = () => {
                                 onSuccess={responseGoogle}
                                 onFailure={failGoogle}
                                 cookiePolicy={'single_host_origin'}
-                            />
-                            {loggedIn ? <Redirect exact to={{ pathname: "/", image: image, isLogged: loggedIn}} /> : ''}
-                            {fail ? <p id="fail-message">Ocorreu algum erro!! Tente Novamente Mais tarde</p> : ''}
+                            /> {/* Google Auth Button */}
+                            {loggedIn ? <Redirect exact to={{ pathname: "/", image: image, isLogged: loggedIn}} /> : ''} {/* if login succeed will redirect to home page */}
+                            {fail ? <p id="fail-message">Ocorreu algum erro!! Tente Novamente Mais tarde</p> : ''} {/** if fails will show this message */}
                         </div>
                     </div>
                 </div>
