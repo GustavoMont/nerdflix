f09f8ec6fd5c308dc65a73fd5dfe4991405f6834
GustavoMont <lgmluisgm@gmail.com>
2021-10-14 10:14:36 -0300

    :pencil: comment Home page code

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { getContent, getTvInfo } from '../Utils/getContent'
-import { GlobalStyle } from '../styles/GlobalStyle'
-import MovieList from '../Components/MovieList'
-import Outdoor from '../Components/Outdoor';
-import Navegation from '../Components/Navegation';
-import { BoxList } from '../styles/ListContainer';
-import Cookies from 'js-cookie'
+import { getContent, getTvInfo } from '../Utils/getContent' // functions to get the content from TMDB API
+import { GlobalStyle } from '../styles/GlobalStyle' // style to all page
+import MovieList from '../Components/MovieList' // Movie/ and tv show list component
+import Outdoor from '../Components/Outdoor'; // Outdoor Component 
+import Navegation from '../Components/Navegation'; // has the title and profile user picture
+import { BoxList } from '../styles/ListContainer'; // COntainer for all movies/tv shows list
+import Cookies from 'js-cookie' // to get user information
 
 const Home = () => {
-    const [content, setContent] = useState([])
-    const [outdoorInfo, setOutdoorInfo] = useState(null)
-    const randomGenerator = (length) => Math.floor(Math.random() * (length - 1))
-    const image = JSON.parse(Cookies.get('teste-nerdflix')).imageUrl
+    const [content, setContent] = useState([]) // it receive all content querried on getContent function
+    const [outdoorInfo, setOutdoorInfo] = useState(null) // this allows to renderize the Outdoor if content is already gotten
+    const randomGenerator = (length) => Math.floor(Math.random() * (length - 1)) // return a random number between 0 and serie list length
+    
+    const image = JSON.parse(Cookies.get('teste-nerdflix')).imageUrl // get image url from user email from cookies seted on login page
     
     useEffect(() => {
         (async () => {
-            const conteudo = await getContent()
+            const conteudo = await getContent() // this function return an object with all lists content
             setContent(conteudo)
             const index = randomGenerator(conteudo[0].lista.length)
-            const info = await getTvInfo(conteudo[0].lista[index].id)
+            const info = await getTvInfo(conteudo[0].lista[index].id) // it gets information about a specific serie randomly chosen
             setOutdoorInfo(info)
         })()
     }, [])
@@ -27,11 +28,13 @@ const Home = () => {
         <>
             <GlobalStyle  />
             
-            <Navegation profilePic={ image }/>
-            {outdoorInfo && <Outdoor content={outdoorInfo} />}
+            <Navegation profilePic={ image }/> 
+            {outdoorInfo && <Outdoor content={outdoorInfo} />} {/*this will render only after the information be gotten */} 
+            
+            {/* For each content gotten will return an MovieList Component with this information */} 
             <BoxList>
                 {
-                    content.map(item => <MovieList content={item} key={item.title} className="movierow"/>)
+                    content.map(item => <MovieList content={item} key={item.title} className="movierow"/> )
                 }
             </BoxList>
         </>
