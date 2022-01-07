345f9116192b4cacf85f3969d92bd8d20b3d08b2
GustavoMont <lgmluisgm@gmail.com>
2022-01-07 18:39:42 -0300

    :tada: add favorite movies as a key on state object
    The information about the favorite moves will be saved on it

--- src/reducer/AppReducer.js
+++ src/reducer/AppReducer.js
@@ -1,11 +1,19 @@
 import { createStore } from 'redux'
 
-const INITIAL_STATE = {menuHome: true}
+const INITIAL_STATE = {menuHome: true, favorites:[], favoritesId:[]}
 
 const reducer = (state = INITIAL_STATE, action) =>{
     switch (action.type) {
         case 'CHANGE_MENU':
-            return { ...state, menuHome: action.menuHome } 
+            return { ...state, menuHome: action.menuHome };
+        case 'ADD_FAVORITES':
+            return { ...state, favorites:[...state.favorites, action.favorite], 
+                favoritesId: [...state.favoritesId, action.favoriteId] }
+        case 'REMOVE_FAVORITE':
+            return {...state, 
+                favorites: state.favorites.filter(item => item.id !== action.delete.id),
+                favoritesId: state.favoritesId.filter(item => item !== action.deleteId),
+            }
         default:
             return state
     }
