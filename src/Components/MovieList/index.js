c6a342e8f93cc22ea5a692ed76af41bbfdaf7267
GustavoMont <lgmluisgm@gmail.com>
2022-01-07 19:00:11 -0300

    :pencil2: update page to use CardMovie Components

--- src/Components/MovieList/index.js
+++ src/Components/MovieList/index.js
@@ -2,29 +2,25 @@ import React, { useState } from 'react'
 import { ListContainer } from '../../styles/ListContainer';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore'; // it's like this "<"
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';// it's like this ">"
-import { imageChecker, handleLeft, handleRight } from '../../Utils/handleMovieList' // all this functions is commented on its files
-import { Link } from 'react-router-dom'
+import { handleLeft, handleRight } from '../../Utils/handleMovieList' // all this functions is commented on its files
+import CardMovie from '../CardMovie';
 
 function MovieList({ content, className }) {
-    const url = `https://image.tmdb.org/t/p/w300` // it's url to get image
     const [x, setX] = useState(-400) // initial position of movie list
 
     return (
         <ListContainer className={className} >
             <h3>{content.title}</h3>
-            <ul style={{ marginLeft: x}} > {/*this make the list move*/}
+            <ul style={{ marginLeft: x }} > {/*this make the list move*/}
                 {content.lista.map(item => (
-                    <li key={`${item.id}`}> 
-                    <Link to={`/info/${content.type}/${item.id}`} > {/* TO get more information about the movie */}
-                        {imageChecker(url, item.poster_path, item.original_title || item.original_name)}
-                    </Link>
-                </li>))}
+                    <CardMovie movie={item} type={content.type} key={item.id} />
+                ))}
             </ul>
             <div className="movieRow-left">
-                <NavigateBeforeIcon style={{ fontSize: 70 }} onClick={() => handleLeft(x, setX) } /> 
+                <NavigateBeforeIcon style={{ fontSize: 70 }} onClick={() => handleLeft(x, setX)} />
             </div>
             <div className="movieRow-rigth">
-                <NavigateNextIcon style={{ fontSize: 70 }}  onClick={() => handleRight(x, setX, content.lista) } />
+                <NavigateNextIcon style={{ fontSize: 70 }} onClick={() => handleRight(x, setX, content.lista)} />
             </div>
         </ListContainer>
     )
