8ebdd78eece9ea565c04c1fb402a17a93c439aac
GustavoMont <lgmluisgm@gmail.com>
2022-01-07 11:00:27 -0300

    :pencil: refactory all code
    Remove bad logic code, useless dependecies.

--- src/Components/MovieList/index.js
+++ src/Components/MovieList/index.js
@@ -5,17 +5,17 @@ import NavigateNextIcon from '@material-ui/icons/NavigateNext';// it's like this
 import { imageChecker, handleLeft, handleRight } from '../../Utils/handleMovieList' // all this functions is commented on its files
 import { Link } from 'react-router-dom'
 
-
 function MovieList({ content, className }) {
     const url = `https://image.tmdb.org/t/p/w300` // it's url to get image
     const [x, setX] = useState(-400) // initial position of movie list
+
     return (
         <ListContainer className={className} >
             <h3>{content.title}</h3>
             <ul style={{ marginLeft: x}} > {/*this make the list move*/}
                 {content.lista.map(item => (
                     <li key={`${item.id}`}> 
-                    <Link to={`/info/${content.type}/${item.id}`}> {/* TO get more information about the movie */}
+                    <Link to={`/info/${content.type}/${item.id}`} > {/* TO get more information about the movie */}
                         {imageChecker(url, item.poster_path, item.original_title || item.original_name)}
                     </Link>
                 </li>))}
